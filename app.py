dc7ad1d7d5a6f721ad291d4801e5a5912e85e258
Dominic Yu <dominicyu@cobaltknitwear.com>
2022-01-20 10:00:25 +0800

    test

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return '21云盒子 Hello Flask 样例'
+
+
+# @app.route('/')
+# def hello_world():
+#     return '21云盒子 Hello Flask 样例'
+
+
+# class test(Resource):
+#     @jwt_required()
+#     def post(self):
+#
+#         return {"result": "fuck"}
+
+
+# api.add_resource(TEST2, '/test2')
+
+# if __name__ == '__main__':
+#     app.run()  # important to mention debug=True
